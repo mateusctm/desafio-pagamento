f1b0c5581c3cc21460b66d921bda995896792de5
mateusctm <mateusctm@gmail.com>
2025-01-28 09:31:32 -0300

    firt commit

---
+++ src/main/java/com/desafio_pagamento/desafio_pagamento/DesafioPagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio_pagamento.desafio_pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio_pagamento/desafio_pagamento/DesafioPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio_pagamento.desafio_pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
